eaa5e1fd0a23173611d847f0281dcc612c89f17f
Stanislav Karkavin <me@xdefrag.dev>
2025-08-21 12:23:44 +0200

    feat: add stellar.toml

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+Bun.serve({
+  routes: {
+    "/": new Response("OK"),
+    "/.well-known/stellar.toml": new Response(
+      await Bun.file("./public/.well-known/stellar.toml").bytes(),
+      {
+        headers: {
+          "Content-Type": "text/plain; charset=utf-8",
+          "Access-Control-Allow-Origin": "*",
+        },
+      },
+    ),
+  },
+  port: process.env.PORT || "3000",
+});
